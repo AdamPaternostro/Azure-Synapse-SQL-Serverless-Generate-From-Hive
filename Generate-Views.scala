35d7857fd87bbcbf93609320b5a81c68ca30a843
Adam Paternostro <adam.paternostro@outlook.com>
2020-06-30 14:37:02 -0400

    Initial check-in

---
+++ Generate-Views.scala
@@ -0,0 +1,121 @@
+/*
+Run this code from a Spark notebook (Scala)
+*/
+
+val dfTable = spark.sql("SHOW TABLES").filter("tableName == 'REPLACE-WITH-TABLE-NAME'") // you can run for all tables, but better to test with a few first
+  
+for (row <- dfTable.collect()) //.take(5))
+{   
+    var sqlOD = new StringBuilder(); 
+    var sqlODPartitionSelect = new StringBuilder(); 
+    var sqlODPartitionLocation = new StringBuilder(); 
+    sqlOD.clear()
+    sqlODPartitionSelect.clear()
+    sqlODPartitionLocation.clear()
+
+    var database = row.mkString(",").split(",")(0)
+    var tableName = row.mkString(",").split(",")(1)
+
+    val location = spark.sql("DESCRIBE EXTENDED " + tableName).filter("col_name == 'Location'").select($"data_type")
+    
+    sqlOD.append("-----------------------------------------------------------------------------------------------\n")
+    sqlOD.append("-- " + tableName + "\n")
+    sqlOD.append("-----------------------------------------------------------------------------------------------\n")
+    sqlOD.append("IF EXISTS (SELECT * FROM sys.objects WHERE name ='" + tableName + "')\n")
+    sqlOD.append("  BEGIN\n")
+    sqlOD.append("  DROP VIEW " + tableName + ";\n")
+    sqlOD.append("  END\n")
+    sqlOD.append("GO\n\n")
+    sqlOD.append("CREATE VIEW " + tableName + " AS\n")
+    sqlOD.append("SELECT\n")
+    sqlOD.append("REPLACE_ME_sqlODPartitionSelect")
+    sqlOD.append("  *\n")
+    sqlOD.append("FROM OPENROWSET(\n")
+    sqlOD.append("   BULK '")
+    for (loc <- location.collect())
+    {
+        var abfsPath = loc.mkString(",").split(",")(0)
+        sqlOD.append(abfsPath)
+        sqlOD.append("REPLACE_ME_sqlODPartitionLocation")
+    }
+    sqlOD.append("/*.parquet',\n")
+    sqlOD.append("   FORMAT='PARQUET'\n")
+    sqlOD.append(")\n")
+    sqlOD.append("WITH (\n")
+
+    val cols = spark.sql("DESCRIBE " + tableName)
+    var i = 1;
+    var partitionIndex = 1;
+    var numberOfColumns = cols.collect().length
+    var processingPartitions = false
+
+    for (col <- cols.collect())
+    {
+        var colString = col.mkString("|")
+        //println(colString)
+        var colSplit = colString.split('|')
+        //colSplit.foreach(println) 
+        var col_name = colSplit(0)
+        var data_type = ""
+        try
+        {
+            data_type = colSplit(1)
+        }
+        catch {
+            case e: Exception => var void = ""
+        }
+
+        // Map data types (MORE WORK needs to be done here, this is just a few mappings)
+        if (data_type == "string")
+        {
+            data_type = "VARCHAR(255)"    // NOTE: This is just a fixed number that was picked since it covered most of the string sizes.  
+        }
+        if (data_type == "double")
+        {
+            data_type = "FLOAT"
+        }
+         if (data_type == "timestamp")
+        {
+            data_type = "DATETIME2"
+        }      
+
+        if (col_name == "# Partition Information" || col_name== "# col_name")
+        {
+            processingPartitions = true;
+        }
+
+        if (processingPartitions == true && col_name != "# Partition Information" && col_name != "# col_name")
+        {
+            sqlODPartitionSelect.append("   CAST(r.filepath(" + partitionIndex.toString() + ") AS " + data_type + ") AS " + col_name + ",\n")
+            sqlODPartitionLocation.append("/" + col_name + "=*")
+            partitionIndex = partitionIndex + 1 
+        }
+        
+        if (processingPartitions == false)
+        {
+            sqlOD.append("  " + col_name + " " + data_type)
+            if (i == numberOfColumns)
+            {
+                sqlOD.append("\n")
+            }
+            else
+            {
+                sqlOD.append(",\n")
+            }
+        }
+        i = i + 1
+    }
+          
+    sqlOD.append(") AS [r];\n")
+    sqlOD.append("GO\n\n")
+    sqlOD.append("-- SELECT TOP 10 * FROM " + tableName + ";\n")
+    sqlOD.append("-----------------------------------------------------------------------------------------------\n")
+    sqlOD.append("\n")
+    sqlOD.append("\n")
+
+    println(sqlOD.toString()
+    .replace("REPLACE_ME_sqlODPartitionSelect",sqlODPartitionSelect.toString())
+    .replace("REPLACE_ME_sqlODPartitionLocation",sqlODPartitionLocation.toString())
+    )
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # Azure-Synapse-SQL-Serverless-Generate-From-Hive
-This is a script that will read a Hive metastore and generate SQL Serverless create view statements.
+This is a script that will read a Hive metastore and generate SQL Serverless CREATE VIEW statements.
+
+## About this script
+- This is a rudimentry script that will perform some code generation to create SQL On-Demand (Serverless) views.  
+- I have customers that have hundreds of Hive tables and they want to start to use SQL OD to query the data
+- This gives a jump start to allow views to be created that will support Hive partitioning
+
+## To run the script
+- Create a Spark notebook in a cluster than can see your Hive metastore
+- Copy the code from Generate-Views.scala
+  - Change the REPLACE-WITH-TABLE-NAME 
+- Run
+- Copy the output to a SQL OD Script window in Synapse
+- Run the generated code
+- Highlight the SELECT TOP 10 * command and ensure the generated view is working
+
+## What the script does
+- The script will read each Hive table and generate the SQL OD code to create a view
+- The script will also handle partitioned tables (projects the partitioned folders into columns)
+- The script assuming "dbo" schema in your SQL OD database
+- The script assumes that you are using the full ABFS path (not a mount path like some spark vendors)
+- The script assumes are pointing at Parquet file (has not been tested for CSV, etc.)
+- Ths script does not handle nested Parquet types or array (you can code it up: https://docs.microsoft.com/en-us/azure/synapse-analytics/sql/query-parquet-nested-types#access-elements-from-nested-columns)
+
+## Needs more work
+- Currently this works with views, but ideally the view would be decomposed into the corrisponding SQL OD statement.  This would avoid having a view call a view (since we are generated a View from a Hive Table)
+- The data type mapping is light and needs more work
+- Strings do not have length in parquet files.  Ideally, a Spark job would run and find the max length of each string and store this information so this script could consume.  Right now it is hardcoded to VARCAHAR(255)
+
+## Data Types (Tips)
+- To see what data types SQL OD is inferring you run this statement, substituting your SELECT statement.  This will help you in mapping additional data types.
+   - ```EXEC sp_describe_first_result_set N'SELECT TOP 100 * FROM OPENROWSET(BULK ''https://STORAGE-ACCOUNT.dfs.core.windows.net/CONTAINER/FOLDER/MY-FILE.parquet'',FORMAT=''PARQUET'') AS [r]'```
+
+- You can also use parquet-tools for viewing parquet meta-data
+   - https://github.com/apache/parquet-mr/tree/master/parquet-tools
+   - ```java -jar parquet-tools-1.12.0-SNAPSHOT.jar meta "us-states.parquet"```
+
+
+## Sample generated code
+```
+-----------------------------------------------------------------------------------------------
+-- nytaxi_green
+-----------------------------------------------------------------------------------------------
+IF EXISTS (SELECT * FROM sys.objects WHERE name ='nytaxi_green')
+  BEGIN
+  DROP VIEW nytaxi_green;
+  END
+GO
+
+CREATE VIEW nytaxi_green AS
+SELECT
+  *
+FROM OPENROWSET(
+   BULK 'abfss://data@mystorageaccount.dfs.core.windows.net/synapse/workspaces/folder/nytaxi_green/*.parquet',
+   FORMAT='PARQUET'
+)
+WITH (
+  vendorID int,
+  lpepPickupDatetime DATETIME2,
+  lpepDropoffDatetime DATETIME2,
+  passengerCount int,
+  tripDistance FLOAT,
+  puLocationId VARCHAR(255),
+  doLocationId VARCHAR(255),
+  pickupLongitude FLOAT,
+  pickupLatitude FLOAT,
+  dropoffLongitude FLOAT,
+  dropoffLatitude FLOAT,
+  rateCodeID int,
+  storeAndFwdFlag VARCHAR(255),
+  paymentType int,
+  fareAmount FLOAT,
+  extra FLOAT,
+  mtaTax FLOAT,
+  improvementSurcharge VARCHAR(255),
+  tipAmount FLOAT,
+  tollsAmount FLOAT,
+  ehailFee FLOAT,
+  totalAmount FLOAT,
+  tripType int
+) AS [r];
+GO
+
+-- SELECT TOP 10 * FROM nytaxi_green;
+-----------------------------------------------------------------------------------------------
+```
+
+## References
+- https://docs.microsoft.com/en-us/azure/synapse-analytics/sql/query-data-storage
